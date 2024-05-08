901bc1cd70472acc0ca700398ce14668768f6912
Lucas Magalhães <imlucasmagalhaes@outlook.com>
2024-05-08 11:24:15 -0300

    hit attempts

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const randomNumber = Math.floor(Math.random() * 100) + 1 //gera um número aleatório entre 1 e 100
 console.log(randomNumber)
 let maxHealth = 10 //define um valor para minha saúde máxima
+let numberOfAttempts = 1 //contaddor de tentativas
 const span = document.getElementById('maxHealth') //seleciona o meu span que vai ser alterado toda vez que o jogador errar o número
 
 const allowedKeys = ["9", "8", "7", "6", "5", "4", "3", "2", "1", "0"] //teclas permitidas no input
@@ -42,6 +43,7 @@ function remainingHealth(){
         console.log('acertou')
     } else { //se o número for diferente ele subtrai 1 do valor máximo e muda o valor dentro do span
         maxHealth--
+        numberOfAttempts++
         span.textContent = ' '
         span.textContent = `${maxHealth}`
         listNumbersPlayed()
@@ -72,14 +74,38 @@ function listNumbersPlayed(){
     ul.appendChild(li)
 }
 
+//recarrega minha página
+function reloadPage(){
+    location.reload()
+    input.value = ''
+}
+
 //captura o botão do meu formulário para o evento de submit
 document.querySelector('form').addEventListener('submit', function(ev){
     ev.preventDefault() //impede o comportamento padrão do meu formulário
     //se o valor do input for diferente de vazio ele chama a minha função para verificar os valores
     if(input.value === ''){
         alert('Insira um valor válido')
+        return
+    } 
+
+    checkNumber() //chama minha função para verificar o número
+    
+    //se ela retornar verdadeira entra nessa condição que vai mostrar a tela de vitória
+    if(checkNumber()){
+
+        const screen1 = document.getElementsByClassName('screen1')[0] //seleciona o meu primeiro elemento da classe screen1
+        screen1.classList.add('hide') //adiciona a classe hide no screen1
+
+        const screen2 = document.getElementsByClassName('screen2')[0] //seleciona o meu primeiro elemento da classe screen2
+        screen2.classList.remove('hide')
+
+        const attempt = document.getElementById('attempt') //seleciona meu span para mostrar a quantidade de tentativas
+        attempt.innerText = `${numberOfAttempts}` //muda o texto que está no meu span
+
+        const reloadBtn = document.getElementById('reload') //captura o meu botão
+        reloadBtn.addEventListener('click', reloadPage) //recarrega a página quando o botão for clicado
     } else {
-        checkNumber()
-        remainingHealth()
+        remainingHealth() //chama a função que vai mostrar a quantidade dde tentativas que restam
     }
 })
